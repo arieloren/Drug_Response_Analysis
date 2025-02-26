23fa682def95a2864ce5946813980981478ad68a
4arielo <4arielo@gmail.com>
2025-02-26 19:56:26 +0200

    Updeting functions for preprocessing

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -31,20 +31,34 @@ def concat_metadata_with_gene_expression(metadata, gen_df, filter_nan_target=Tru
     
     return merged_df
 
-def prepare_dataset_for_feauture_selection(df):
+def prepare_dataset_for_feature_selection(df):
     """
-    Prepares the dataset by:
-    - Dropping non-numeric columns.
-    - Converting Response status to binary.
-    - Handling missing values.
-    """
-    drop_cols = ["SampleID", "Tissue", "disease state", "protocol", "disease activity score (das28)", "Gender"]
-    df = df.drop(columns=drop_cols, errors="ignore")
+    Prepares the dataset for feature selection by:
+    - Converting the "Response status" column to binary values (1 = Responder, 0 = Non-responder).
+    - Separating features (X) and target variable (y).
+    - Dropping non-numeric and irrelevant columns that are not useful for feature selection.
 
+    Parameters:
+    df (pd.DataFrame): The input dataset containing gene expression data and response labels.
+
+    Returns:
+    X (pd.DataFrame): The feature matrix containing gene expression values.
+    y (pd.Series): The target variable (treatment response) encoded as 0 (Non-responder) or 1 (Responder).
+    """
+    
     # Convert Response status to binary (1 = Responder, 0 = Non-responder)
     df["Response status"] = df["Response status"].map({"Responder": 1, "Non_responder": 0})
 
-    return df
+    # Separate features (X) and target variable (y)
+    X = df.drop(columns=["Response status"])
+    y = df["Response status"].astype(int)
+
+    # Drop irrelevant columns
+    drop_cols = ["SampleID", "Tissue", "disease state", "protocol", "disease activity score (das28)", "Gender"]
+    X = X.drop(columns=drop_cols, errors="ignore")
+    
+    return X, y
+
 
 def normalize_features(X):
     """
